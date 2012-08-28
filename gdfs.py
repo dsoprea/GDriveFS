78aa83a3d254eb67c0472a1dc53b903708692b8d
Dustin Oprea <myselfasunder@gmail.com>
2012-08-28 03:39:12 -0400

    We've replace fuse-python with fusepy. destroy() is now actually being called, invocation seems a little more straight-forward, and I can [easily] even choose not to disconnect from the terminal in order to debug. This will allow us to see Python errors, use PDB to debug, and to develop multithreaded functionality with a lighter heart.

--- gdfs.py
+++ gdfs.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-import fuse
 import stat
 import logging
 import dateutil.parser
@@ -10,37 +9,39 @@ import errno
 
 from time       import mktime
 from argparse   import ArgumentParser
+from fuse       import FUSE, Operations
+from sys        import argv
 
 from utility import get_utility
 from gdrivefs.cache import PathRelations
 
-if not hasattr(fuse, '__version__'):
-    raise RuntimeError, \
-        "Your fuse-py doesn't know of fuse.__version__, probably it's too old."
-
-fuse.fuse_python_api = (0, 2)
+#if not hasattr(fuse, '__version__'):
+#    raise RuntimeError, \
+#        "Your fuse-py doesn't know of fuse.__version__, probably it's too old."
+#
+#fuse.fuse_python_api = (0, 2)
 
 app_name = 'GDriveFS Tool'
 
-class _GDriveStat(fuse.Stat):
-    """A skeleton stat() structure."""
+#class _GDriveStat(fuse.Stat):
+#    """A skeleton stat() structure."""
+#
+#    def __init__(self):
+#        self.st_mode = 0
+#        self.st_ino = 0
+#        self.st_dev = 0
+#        self.st_nlink = 0
+#        self.st_uid = 0
+#        self.st_gid = 0
+#        self.st_size = 0
+#        self.st_atime = 0
+#        self.st_mtime = 0
+#        self.st_ctime = 0
 
-    def __init__(self):
-        self.st_mode = 0
-        self.st_ino = 0
-        self.st_dev = 0
-        self.st_nlink = 0
-        self.st_uid = 0
-        self.st_gid = 0
-        self.st_size = 0
-        self.st_atime = 0
-        self.st_mtime = 0
-        self.st_ctime = 0
-
-class _GDriveFS(fuse.Fuse):
+class _GDriveFS(Operations):
     """The main filesystem class."""
 
-    def getattr(self, path):
+    def getattr(self, path, fh=None):
         """Return a stat() structure."""
 
         logging.info("Stat() on [%s]." % (path))
@@ -57,36 +58,38 @@ class _GDriveFS(fuse.Fuse):
         is_folder = get_utility().is_directory(entry_clause[0])
         entry = entry_clause[0]
 
-        st = _GDriveStat()
+#        st = _GDriveStat()
+
+#        return dict(st_mode=(S_IFDIR | 0755), st_ctime=now,
+#            st_mtime=now, st_atime=now, st_nlink=2)
+
 
         if is_folder:
-            st.st_mode = stat.S_IFDIR | 0755
-            st.st_nlink = 2
-
+            return {
+                    "st_mode": (stat.S_IFDIR | 0755),
+                    "st_nlink": 2
+                }
         else:
-            st.st_mode = stat.S_IFREG | 0444
-            st.st_nlink = 1
-            st.st_size = int(entry.quota_bytes_used)
-
             date_obj = dateutil.parser.parse(entry.modified_date)
             mtime_epoch = mktime(date_obj.timetuple())
-            st.st_mtime = mtime_epoch
 
-#        else:
-#            return -errno.ENOENT
+            return {
+                    "st_mode": (stat.S_IFREG | 0444),
+                    "st_nlink": 1,
+                    "st_size": int(entry.quota_bytes_used),
+                    "st_mtime": mtime_epoch
+                }
 
-        return st
-
-    def readdir(self, path, offset):
+    def readdir(self, path, fh):
         """A generator returning one base filename at a time."""
 
-        logging.info("ReadDir(%s,%d) invoked." % (path, offset))
+        logging.info("ReadDir(%s,%s) invoked." % (path, type(fh)))
 
         # We expect "offset" to always be (0).
-        if offset != 0:
-            logging.warning("readdir() has been invoked for path [%s] and non-"
-                            "zero offset (%d). This is not allowed." % 
-                            (path, offset))
+#        if offset != 0:
+#            logging.warning("readdir() has been invoked for path [%s] and non-"
+#                            "zero offset (%d). This is not allowed." % 
+#                            (path, offset))
 
 # TODO: Return -ENOENT if not found?
 # TODO: Once we start working on the cache, make sure we don't make this call, 
@@ -113,7 +116,7 @@ class _GDriveFS(fuse.Fuse):
         filenames[0:0] = ['.','..']
         logging.info(filenames)
         for filename in filenames:
-            yield fuse.Direntry(filename)
+            yield filename
 
 #    def fsinit(*args): 
 #        import syslog
@@ -121,14 +124,16 @@ class _GDriveFS(fuse.Fuse):
 #        syslog.syslog("INIT") 
 #        syslog.closelog() 
 #
-#    def destroy(self, path):
+#    def __del__(self, *args):
 #        with open('/tmp/destroy', 'w') as f:
 #            f.write("Content.")
 #
-#        import syslog
-#        syslog.openlog('myfs2') 
-#        syslog.syslog("DESTROY (2)") 
-#        syslog.closelog()
+#    def fsdestroy(self, *args):
+#        with open('/tmp/destroy', 'w') as f:
+#            f.write("Content.")
+    def destroy(self, path):
+        """Called on filesystem destruction. Path is always /"""
+        logging.info("FS destroyed.")
 
 def dump_changes(overview):
     (largest_change_id, next_page_token, changes) = overview
@@ -140,24 +145,12 @@ def dump_changes(overview):
 #    print(changes)
 
 def main():
-    #change_overview = drive_proxy('list_changes')
-    #dump_changes(change_overview)
-    #files = drive_proxy('list_files')
 
-    #return
-    usage="""GDriveFS Fuser\n\n""" + fuse.Fuse.fusage
-    server = _GDriveFS(version="%prog " + fuse.__version__,
-                      usage=usage,
-                      dash_s_do='setsingle')
+    if len(argv) != 2:
+        print 'usage: %s <mountpoint>' % argv[0]
+        exit(1)
 
-#    server.parser.add_option(mountopt='ac', metavar="FILEPATH",
-#                             help="API Credentials JSON file-path.")
-#    server.parser.add_option('-a', '--ac', metavar="FILEPATH", 
-#                             help="API Credentials JSON file-path.")
-
-    fuse_args = server.parse(errex=1)
-
-    server.main()
+    fuse = FUSE(_GDriveFS(), argv[1], foreground=True, nothreads=True)
 
 if __name__ == "__main__":
     main()
