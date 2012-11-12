d2f5dfbe6050c2fcc1f22860d3e1ce252e29c172
Dustin Oprea <myselfasunder@gmail.com>
2012-11-12 01:35:43 -0500

    Built egg. Add pre- and post-install hooks. Still has to be tested.

--- setup.py
+++ setup.py
@@ -1,24 +1,79 @@
-#!/usr/bin/python
+from setuptools import setup, find_packages
+import sys, os
 
 from os import symlink
 from os.path import dirname
 
 import tools
 
-tool_path = dirname(tools.__file__)
+def pre_install():
 
-gdfs_filepath = ('%s/%s' % (tool_path, 'gdfs'))
-gdfs_symlink_filepath = '/usr/sbin/gdfs'
+# TODO: Ensure FUSE.
+    return True
 
-gdfstool_filepath = ('%s/%s' % (tool_path, 'gdfstool'))
-gdfstool_symlink_filepath = '/usr/sbin/gdfstool'
+def post_install():
+    tool_path = dirname(tools.__file__)
 
-print("Writing gdfs symlink.")
-symlink(gdfs_symlink_filepath, gdfs_symlink_filepath)
+    gdfs_filepath = ('%s/%s' % (tool_path, 'gdfs'))
+    gdfs_symlink_filepath = '/usr/sbin/gdfs'
 
-print("Writing gdfstool symlink.")
-symlink(gdfstool_filepath, gdfstool_symlink_filepath)
+    gdfstool_filepath = ('%s/%s' % (tool_path, 'gdfstool'))
+    gdfstool_symlink_filepath = '/usr/sbin/gdfstool'
 
+    print("Writing gdfs symlink.")
+    symlink(gdfs_symlink_filepath, gdfs_symlink_filepath)
 
-# Ensure FUSE.
+    print("Writing gdfstool symlink.")
+    symlink(gdfstool_filepath, gdfstool_symlink_filepath)
+
+if not pre_install():
+    sys.exit(1)
+
+class custom_install(install):
+    def run(self):
+        install.run(self)
+
+        post_install()
+
+version = '0.1'
+
+setup(name='gdrivefs',
+      version=version,
+      description="A complete FUSE adapter for Google Drive.",
+      long_description="""\
+A complete FUSE adapter for Google Drive. See Github for more information.""",
+      classifiers=['Topic :: System :: Filesystems',
+                   'Development Status :: 2 - Pre-Alpha',
+                   'Environment :: Console',
+                   'Intended Audience :: End Users/Desktop',
+                   'Intended Audience :: System Administrators',
+                   'License :: OSI Approved :: BSD License',
+                   'Natural Language :: English',
+                   'Operating System :: POSIX',
+                   'Programming Language :: Python',
+                   'Topic :: Internet',
+                   'Topic :: Utilities',
+                  ],
+      keywords='google-drive google drive fuse filesystem',
+      author='Dustin Oprea',
+      author_email='myselfasunder@gmail.com',
+      url='https://github.com/dsoprea/GDriveFS',
+      license='New BSD',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'ez_setup',
+        'google_appengine',
+        'google_api_python_client',
+        'httplib2',
+        'python-dateutil',
+        'fusepy',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      cmdclass={'install': custom_install
+               },
+      )
 
