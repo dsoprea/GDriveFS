49497079017c8b7eb3f48c57a87da2f9045c4a67
Dustin Oprea <myselfasunder@gmail.com>
2013-03-11 21:12:42 -0400

    Turned-off physical log. Reenabled syslog.

--- gdrivefs/log_config.py
+++ gdrivefs/log_config.py
@@ -1,23 +1,24 @@
-from logging import getLogger, Filter, Formatter, DEBUG
+from logging import getLogger, Filter, Formatter, DEBUG, WARNING
 from logging.handlers import SysLogHandler, TimedRotatingFileHandler
 from os.path import abspath, dirname
 
 import gdrivefs
 
 default_logger = getLogger()
-default_logger.setLevel(DEBUG)
+default_logger.setLevel(WARNING)
 
-# TODO: For errors: SMTPHandler
+# Log to syslog.
 
-root_path = abspath(dirname(gdrivefs.__file__) + '/..')
-log_filepath = ('%s/logs/gdrivefs.log' % (root_path))
+log_syslog = SysLogHandler('/dev/log', facility=SysLogHandler.LOG_LOCAL0)
+log_format = 'GD: %(name)-12s %(levelname)-7s %(message)s'
+log_syslog.setFormatter(Formatter(log_syslog))
+default_logger.addHandler(log_syslog)
 
-#syslog_format = 'MC:%(name)s %(levelname)s %(message)s'
-flat_format = '%(asctime)s [%(name)s %(levelname)s] %(message)s'
+# Log to physical file.
 
-formatter = Formatter(flat_format)
-
-log_file = TimedRotatingFileHandler(log_filepath, 'D', backupCount=5)
-log_file.setFormatter(formatter)
-default_logger.addHandler(log_file)
+#root_path = abspath(dirname(gdrivefs.__file__) + '/..')
+#log_filepath = ('%s/logs/gdrivefs.log' % (root_path))
+#log_file = TimedRotatingFileHandler(log_filepath, 'D', backupCount=5)
+#log_file.setFormatter(Formatter('%(asctime)s [%(name)s %(levelname)s] %(message)s'))
+#default_logger.addHandler(log_file)
 
