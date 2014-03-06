7d2297e842f19666cc568909c2a0066cb4b45408
Dustin Oprea <myselfasunder@gmail.com>
2014-03-06 09:45:10 -0500

    Added test-harness.

---
+++ dev/test_harness.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python2.7
+
+import sys
+sys.path.insert(0, '.')
+
+from pprint import pprint
+
+from gdrivefs.gdfs.gdfuse import set_auth_cache_filepath
+from gdrivefs.gdtool.drive import GdriveAuth
+
+auth = GdriveAuth()
+client = auth.get_client()
+
+#print(dir(client.files()))
+#sys.exit()
+
+#response = client.about().get().execute()
+#request = client.files().media_get()
+#response = client.files().list().execute()
+response = client.files().get(fileId='1xxGrmEAv4-2ZM1MYj4UXpnxUp73d2VmtI9TdFERrSbM').execute()
+
+pprint(response.keys())
+
+#for entry in response['items']:
+#    pprint(dir(entry))
+#    sys.exit()
+
+#pprint(dir(response))
+
