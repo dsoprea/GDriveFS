3d2173199f3b4d23427e71c6f0c4ecc5ab8c9de9
Dustin Oprea <myselfasunder@gmail.com>
2014-03-06 00:24:39 -0500

    Added connection pool implementation.
    Not used yet.

---
+++ gdrivefs/http_pool.py
@@ -0,0 +1,29 @@
+from contextlib import contextmanager
+
+from geventhttpclient import httplib
+from gevent.queue import Queue
+
+httplib.patch()
+
+from httplib2 import Http
+
+
+class HttpPool(object):
+    def __init__(self, size, factory=Http):
+        self.__size = size
+        self.__factory = factory
+        self.__pool = Queue()
+
+        for i in xrange(self.__size):
+            self.__pool.put(self.__factory())
+
+    @contextmanager
+    def reserve(self):
+        http = self.__pool.get()
+        yield http
+        self.__pool.put(http)
+
+    def request(self, *args, **kwargs):
+        with self.reserve() as http:
+            return http.request(*args, **kwargs)
+
