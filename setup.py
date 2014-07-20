b97c69d4e42df6bc41689ddc92528d37e3b3636e
Dustin Oprea <myselfasunder@gmail.com>
2014-07-20 16:12:33 -0400

    Formalized packaging.
    - Repositioned tools as scripts, in a "scripts" directory.
    - Refactored Markdown as RST.
    - We now use the requirements file and README file to populate those respective components of the setuptools structure.
    - Wrote resource files into MANIFEST.in .
    - Deimplemented Versioneer.

--- setup.py
+++ setup.py
@@ -1,54 +1,49 @@
 #!/usr/bin/env python2.7
 
-from setuptools import find_packages, setup
+import setuptools
+import os.path
 
-import versioneer
+import gdrivefs
 
-versioneer.VCS = 'git'
-versioneer.versionfile_source = 'gdrivefs/_version.py'
-versioneer.versionfile_build = 'gdrivefs/_version.py'
-versioneer.tag_prefix = ''
-versioneer.parentdir_prefix = 'gdrivefs-'
+_APP_PATH = os.path.dirname(gdrivefs.__file__)
 
-long_description = "A complete FUSE adapter for Google Drive. See Github for "\
-                   "more information."
+with open(os.path.join(_APP_PATH, 'resources', 'README.rst')) as f:
+      long_description = f.read()
 
-setup(name='gdrivefs',
-      version=versioneer.get_version(),
-      description="A complete FUSE adapter for Google Drive.",
-      long_description=long_description,
-      classifiers=['Topic :: System :: Filesystems',
-                   'Development Status :: 4 - Beta',
-                   'Environment :: Console',
-                   'Intended Audience :: End Users/Desktop',
-                   'Intended Audience :: System Administrators',
-                   'License :: OSI Approved :: BSD License',
-                   'Natural Language :: English',
-                   'Operating System :: POSIX',
-                   'Programming Language :: Python',
-                   'Topic :: Internet',
-                   'Topic :: Utilities'],
-      keywords='google-drive google drive fuse filesystem',
-      author='Dustin Oprea',
-      author_email='myselfasunder@gmail.com',
-      url='https://github.com/dsoprea/GDriveFS',
-      license='GPL 2',
-      packages=find_packages(exclude=['dev','tests']),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-        'google_appengine',
-# TODO: There's an issue when this is listed as a requirement here. It can be 
-#       installed separately, easily. The repository version is old, anyways.
-#       We advise that people download and install it manually.
-#        'google_api_python_client',
-        'httplib2',
-        'python-dateutil',
-        'fusepy',
-      ],
-      cmdclass=versioneer.get_cmdclass(),
-      scripts=['tools/gdfs',
-               'tools/gdfstool',
-               'tools/gdfsuninstall'],
-      )
+with open(os.path.join(_APP_PATH, 'resources', 'requirements.txt')) as f:
+      install_requires = list(map(lambda s: s.strip(), f.readlines()))
+
+classifiers = [
+    'Topic :: System :: Filesystems',
+    'Development Status :: 4 - Beta',
+    'Environment :: Console',
+    'Intended Audience :: End Users/Desktop',
+    'Intended Audience :: System Administrators',
+    'License :: OSI Approved :: BSD License',
+    'Natural Language :: English',
+    'Operating System :: POSIX',
+    'Programming Language :: Python',
+    'Topic :: Internet',
+    'Topic :: Utilities',
+]
+
+setuptools.setup(
+    name='gdrivefs',
+    version=gdrivefs.__version__,
+    description="A complete FUSE adapter for Google Drive.",
+    long_description=long_description,
+    classifiers=classifiers,
+    keywords='google-drive google drive fuse filesystem',
+    author='Dustin Oprea',
+    author_email='myselfasunder@gmail.com',
+    url='https://github.com/dsoprea/GDriveFS',
+    license='GPL 2',
+    packages=setuptools.find_packages(exclude=['dev','tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    scripts=['gdrivefs/resources/scripts/gdfs',
+             'gdrivefs/resources/scripts/gdfstool',
+             'gdrivefs/resources/scripts/gdfsuninstall'],
+)
 
