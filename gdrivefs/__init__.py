c461e30dbe5434633564700727ee45dd3c6d1e06
Dustin Oprea <myselfasunder@gmail.com>
2014-12-09 02:08:42 -0500

    Version bump.

--- gdrivefs/__init__.py
+++ gdrivefs/__init__.py
@@ -1,5 +1,1 @@
-# TODO(dustin): Why is this here?
-#import collections
-#TypedEntry = collecitons.namedtuple('TypedEntry', ['entry_id', 'mime_type'])
-
-__version__ = '0.13.14'
+__version__ = '0.14.0'
