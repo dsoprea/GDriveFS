602c4c20589eae1acf92c7a58aa916858ede6fcd
Dustin Oprea <myselfasunder@gmail.com>
2017-12-04 02:01:16 -0500

    Vagrant-related changes.
    - Bumped a dependency version to support Vagrant.
    - Tweaked Vagrant provision to not require SUDO.

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -11,25 +11,30 @@ apt-get install -y python-pip build-essential python-dev
 
 # Install dependencies.
 
-pip install ez_setup
+pip install -U pip
 pip install -r /gdrivefs/requirements.txt
 
 # Add "gdrivefs" to PYTHONPATH and install executables into path.
 
-cd /gdrivefs
-python setup.py develop
+pip install -e /gdrivefs
 
 # Create a mountpoint.
 #
-# We can't use /mnt/gdrivefs, because, for some reason, when we unmount that in 
+# We can't use /mnt/gdrivefs, because, for some reason, when we unmount that in
 # the Vagrant machine, it unmounts the shared-folder the the same name, too.
 
 mkdir /mnt/g
+sudo chown vagrant:vagrant /mnt/g
+
+# Configure us to run as non-root.
+
+sudo chmod 755 /etc/fuse.conf
+sudo adduser vagrant fuse
 
 # Get the user going with an authorization URL.
 
 echo
-gdfstool auth -u
+gdfstool auth_get_url
 
 # Mention follow-up steps.
 # *For some reason, we can't print empty strings, so we print single-spaces.
@@ -37,15 +42,15 @@ gdfstool auth -u
 echo " "
 echo "Once you have retrieved your authorization string, run:"
 echo " "
-echo "  sudo gdfstool auth -a /var/cache/gdfs.creds <auth string>"
+echo "  gdfstool auth_write <authcode>"
 echo " "
 echo " "
 echo "Once authorized, you can mount by calling (as root):"
 echo " "
-echo "  sudo gdfs /var/cache/gdfs.creds /mnt/g"
+echo "  gdfs default /mnt/g"
 echo " "
 echo "Or, to debug (and disable change-monitoring, to simplify things):"
 echo " "
-echo "  sudo GD_DEBUG=1 GD_MONITOR_CHANGES=0 gdfs /var/cache/gdfs.creds /mnt/g"
+echo "  GD_DEBUG=1 GD_MONITOR_CHANGES=0 gdfs default /mnt/g"
 echo " "
 echo " "
